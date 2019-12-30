28aea420ce015128446b188413e72947c853353a
thannedel <57347092+thannedel@users.noreply.github.com>
2019-12-30 13:37:17 +0100

    init

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -4,62 +4,36 @@
     <button onclick="writePosts()">Send post</button>-->
 
     <!-- <form @submit.prevent="addMessage"></form> -->
-    <b-input-group class="mt-3">
-      <b-form-input name="new-message" v-model="newMessage" placeholder="Type your message"></b-form-input>
 
-      <b-button variant="info" type="button" id="send" @click="addMessage()">Send Post</b-button>
-    </b-input-group>
-    <div v-if="!isLoading">
-      <p v-for="(chat, index) in chats" v-bind:key="index">{{chat.name}} {{chat.message}}</p>
-    </div>
-  </div>
-              <div class="incoming_msg">
-                <div class="incoming_msg_img">
-                  <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-                </div>
-                <div class="received_msg">
-                  <div class="received_withd_msg">
-                    <p>Test, which is a new approach to have</p>
-                    <span class="time_date">11:01 AM | Yesterday</span>
+    <div class="container">
+      <div class="messaging">
+        <div class="inbox_msg">
+          <div class="inbox_people"></div>
+
+          <div class="mesgs">
+            <div class="msg_history">
+              <div v-if="!isLoading">
+                <div v-for="(chat, index) in chats" v-bind:key="index" class="incoming_msg">
+                  <div class="incoming_msg_img">
+                    <!-- <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> -->
+                  </div>
+                  <div :class="[chat.name===userName?'sent_msg':'received_msg']">
+                    <div class="received_withd_msg">
+                      <p>{{chat.message}}</p>
+                      <span class="time_date">{{chat.name}}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="outgoing_msg">
-                <div class="sent_msg">
-                  <p>Apollo University, Delhi, India Test</p>
-                  <span class="time_date">11:01 AM | Today</span>
-                </div>
-              </div>
-              <div class="incoming_msg">
-                <div class="incoming_msg_img">
-                  <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-                </div>
-                <div class="received_msg">
-                  <div class="received_withd_msg">
-                    <p>
-                      We work directly with our designers and suppliers,
-                      and sell direct to you, which means quality, exclusive
-                      products, at a price anyone can afford.
-                    </p>
-                    <span class="time_date">11:01 AM | Today</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="type_msg">
-              <div class="input_msg_write">
-                <input type="text" class="write_msg" placeholder="Type a message" />
-                <button class="msg_send_btn" type="button">
-                  <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-                </button>
               </div>
             </div>
           </div>
-      
-        <p class="text-center top_spac">
-          Design by
-          <a target="_blank" href="#">Sunil Rajput</a>
-        </p>
+
+          <b-input-group class="mt-3">
+            <b-form-input name="new-message" v-model="newMessage" placeholder="Type your message"></b-form-input>
+
+            <b-button variant="info" type="button" id="send" @click="addMessage()">Send Post</b-button>
+          </b-input-group>
+        </div>
       </div>
     </div>
   </div>
@@ -133,4 +107,75 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  max-width: 1170px;
+  margin: auto;
+}
+img {
+  max-width: 100%;
+}
+.inbox_people {
+  background: #f8f8f8 none repeat scroll 0 0;
+  float: left;
+  overflow: hidden;
+  width: 40%;
+  border-right: 1px solid #c4c4c4;
+}
+.inbox_msg {
+  border: 1px solid #c4c4c4;
+  clear: both;
+  overflow: hidden;
+}
+
+.chat_img {
+  float: left;
+  width: 11%;
+}
+
+.incoming_msg_img {
+  display: inline-block;
+  width: 6%;
+}
+.received_msg {
+  display: inline-block;
+  padding: 0 0 0 60%;
+  vertical-align: top;
+  width: 100%;
+}
+.received_withd_msg p {
+  background: #ebebeb none repeat scroll 0 0;
+  border-radius: 3px;
+  color: #646464;
+  font-size: 14px;
+  margin: 0;
+  padding: 5px 10px 5px 12px;
+  width: 100%;
+}
+.time_date {
+  color: #747474;
+  display: block;
+  font-size: 12px;
+  margin: 8px 0 0;
+}
+.received_withd_msg {
+  width: 57%;
+}
+.mesgs {
+  float: left;
+  padding: 30px 15px 0 25px;
+  width: 100%;
+}
+
+.outgoing_msg {
+  overflow: hidden;
+  margin: 26px 0 26px;
+}
+
+.messaging {
+  padding: 0 0 50px 0;
+}
+.msg_history {
+  height: 516px;
+  overflow-y: auto;
+}
 </style>
